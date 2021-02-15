00788ec40957b4894b2775bd346e48694ec700f0
Spencer <spencer.haka@gmail.com>
2021-02-14 20:10:07 -0700

    More changes

--- Small_Programs/mortgage.js
+++ Small_Programs/mortgage.js
@@ -8,22 +8,34 @@
 
 let rdline = require('readline-sync');
 
+function valNum(number) {
+  while (number.trimStart() === '' || Number.isNaN(Number(number))) {
+    console.log(`Please enter a valid integer.`);
+    number = rdline.question();
+  }
+  return number;
+}
+
 console.log('Welcome to the Loan Calculator!');
 
 console.log('What is the loan amount?');
-let lnAmnt = Number(rdline.question());
+let lnAmnt = rdline.question();
+valNum(lnAmnt);
 
 console.log('What is the interest rate?');
-let intRt = rdline.question() * .01 / 12;
-while ((Number.isNaN(Number(intRt))) || (intRt.typeOf === 'string')) {
+let intRt = rdline.question();
+while (Number.isNaN(Number(intRt)) || intRt.trimStart() === '') {
   console.log(`Please enter a valid integer and exlude symbols.`);
-  intRt = rdline.question() * .01 / 12;
+  intRt = rdline.question();
 }
 
 console.log('What is the duration of the loan in months?');
-let duration = Number(rdline.question());
+let duration = rdline.question();
+valNum(duration);
 
-let payment = lnAmnt * (intRt / (1 - Math.pow((1 + intRt), (-duration))));
+let payment = lnAmnt * ((intRt * .01 / 12) /
+  (1 - Math.pow((1 + (intRt * .01 / 12)), (-duration))));
+
 let amount = payment.toFixed(2);
 console.log(`The total monthly payment is $${amount}`);
 
