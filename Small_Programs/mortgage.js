6022e1a3ab1be17a0b56015776350c2ba0e3c453
Spencer <spencer.haka@gmail.com>
2021-02-14 18:47:56 -0700

    Some changes

--- Small_Programs/mortgage.js
+++ Small_Programs/mortgage.js
@@ -13,13 +13,17 @@ console.log('Welcome to the Loan Calculator!');
 console.log('What is the loan amount?');
 let lnAmnt = Number(rdline.question());
 
-console.log('What is the interest rate? Enter the number only.');
-let intRt = Number(rdline.question()) * .01 / 12;
+console.log('What is the interest rate?');
+let intRt = rdline.question() * .01 / 12;
+while ((Number.isNaN(Number(intRt))) || (intRt.typeOf === 'string')) {
+  console.log(`Please enter a valid integer and exlude symbols.`);
+  intRt = rdline.question() * .01 / 12;
+}
 
 console.log('What is the duration of the loan in months?');
 let duration = Number(rdline.question());
 
 let payment = lnAmnt * (intRt / (1 - Math.pow((1 + intRt), (-duration))));
-
 let amount = payment.toFixed(2);
 console.log(`The total monthly payment is $${amount}`);
+
