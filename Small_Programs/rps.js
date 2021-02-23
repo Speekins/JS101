ddb58d49e301b482fa0f3ef09f169c0665a34831
Spencer <spencer.haka@gmail.com>
2021-02-22 20:10:41 -0700

    Refactored code

--- Small_Programs/rps.js
+++ Small_Programs/rps.js
@@ -1,8 +1,6 @@
 const rdln = require('readline-sync');
 const RPS = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
 
-console.clear();
-
 let computerWins = [];
 let userWins = [];
 
@@ -20,18 +18,7 @@ function validChoice(choice) {
 
 function compChoice() {
   let random = Math.floor(Math.random() * 5);
-  switch (true) {
-    case random === 0:
-      return RPS[0];
-    case random === 1:
-      return RPS[1];
-    case random === 2:
-      return RPS[2];
-    case random === 3:
-      return RPS[3];
-    case random === 4:
-      return RPS[4];
-  }
+  return RPS[random];
 }
 
 function displayWinner(choice, comp) {
@@ -55,6 +42,16 @@ function scoreKeeper(result) {
     userWins.push(1);
   } else if (result === 2) {
     computerWins.push(1);
+  }
+}
+
+function grandChampion() {
+  if (userWins.length === 5) {
+    prompt('First to 5! You are the grand champion!');
+    return true;
+  } else if (computerWins.length === 5) {
+    prompt('First to 5! Computer wins!');
+    return true;
   }
 }
 
@@ -82,22 +79,19 @@ while (true) {
 
     prompt(`Your score: ${userWins.length}. Computer score: ${computerWins.length}.`);
 
-    if (userWins.length === 5) {
-      prompt('First to 5! You are the grand champion!');
-      break;
-    } else if (computerWins.length === 5) {
-      prompt('First to 5! Computer wins!');
+    if (grandChampion() === true) {
       break;
     }
   }
-
   prompt(`Would you like to play another round? Hit y or n`);
   let again = validRepeat(rdln.question().toLowerCase());
   if (again[0] === 'n') {
     prompt('See ya!');
+    console.clear();
     break;
   } else if (again[0] === 'y') {
     computerWins = [];
     userWins = [];
+    console.clear();
   }
 }
