4c1000029958a43c355a083511a2f41879ee17d3
Spencer <spencer.haka@gmail.com>
2021-11-02 19:37:02 -0600

    small tweaks to organization of code

--- lesson_6/ttt.js
+++ lesson_6/ttt.js
@@ -1,9 +1,15 @@
-// eslint-disable-next-line max-len
-const WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+const WINNING_COMBOS = [
+  [1, 2, 3], [4, 5, 6],
+  [7, 8, 9], [1, 4, 7],
+  [2, 5, 8], [3, 6, 9],
+  [1, 5, 9], [3, 5, 7]
+];
 const rdln = require('readline-sync');
 const INITIAL_MARKER = ' ';
-const PLAYER_MARKER = 'X';
-const COMPUTER_MARKER = 'O';
+const MIDDLE_SQUARE = 5;
+const MAX_WINS = 5;
+let playerMarker;
+let computerMarker;
 let board;
 let currentPlayer;
 let playerMatchTotal = 0;
@@ -11,18 +17,16 @@ let computerMatchTotal = 0;
 let totalGames = 0;
 
 
-
-
 function alternatePlayer(currentPlayer) {
   if (currentPlayer === 'player') {
     return 'computer';
-  } else { return 'player' };
+  } else { return 'player' }
 }
 
 function attackSquare(line, board) {
   let markersInLine = line.map(square => board[square]);
 
-  if (markersInLine.filter(val => val === COMPUTER_MARKER).length === 2) {
+  if (markersInLine.filter(val => val === computerMarker).length === 2) {
     let openSpot = line.find(square => board[square] === INITIAL_MARKER);
     if (openSpot !== undefined) {
       return openSpot;
@@ -37,33 +41,31 @@ function boardIsFull(board) {
 
 function computerChoosesSquare(board) {
   let square;
+  square = computerStrategyMoves(board);
+  if (!square) {
+    if (board[MIDDLE_SQUARE] === INITIAL_MARKER) {
+      square = MIDDLE_SQUARE;
+    } else {
+      let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+      square = emptySquares(board)[randomIndex];
+    }
+  }
+  board[square] = computerMarker;
+}
 
+function computerStrategyMoves(board) {
+  let square;
   for (let idx = 0; idx < WINNING_COMBOS.length; idx++) {
     let line = WINNING_COMBOS[idx];
-    square = attackSquare(line, board);
-    if (square) break;
-  }
-
-  if (!square) {
-    for (let idx = 0; idx < WINNING_COMBOS.length; idx++) {
-      let line = WINNING_COMBOS[idx];
+    if (attackSquare(line, board)) {
+      square = attackSquare(line, board);
+      break;
+    } else if (findAtRiskSquare(line, board)) {
       square = findAtRiskSquare(line, board);
-      if (square) break;
+      break;
     }
   }
-
-  if (!square) {
-    if (board[5] === INITIAL_MARKER) {
-      square = 5;
-    }
-  }
-
-  if (!square) {
-    let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
-    square = emptySquares(board)[randomIndex];
-  }
-
-  board[square] = COMPUTER_MARKER;
+  return square;
 }
 
 function chooseSquare(board, currentPlayer) {
@@ -72,21 +74,22 @@ function chooseSquare(board, currentPlayer) {
   } else if (currentPlayer === 'computer') {
     return playerChoosesSquare(board);
   }
+  return null;
 }
 
 function decideWinner(board) {
   for (let idx = 0; idx < WINNING_COMBOS.length; idx++) {
     let [sq1, sq2, sq3] = WINNING_COMBOS[idx];
     if (
-      board[sq1] === PLAYER_MARKER &&
-      board[sq2] === PLAYER_MARKER &&
-      board[sq3] === PLAYER_MARKER
+      board[sq1] === playerMarker &&
+      board[sq2] === playerMarker &&
+      board[sq3] === playerMarker
     ) {
       return 'Player';
     } else if (
-      board[sq1] === COMPUTER_MARKER &&
-      board[sq2] === COMPUTER_MARKER &&
-      board[sq3] === COMPUTER_MARKER
+      board[sq1] === computerMarker &&
+      board[sq2] === computerMarker &&
+      board[sq3] === computerMarker
     ) {
       return 'Computer';
     }
@@ -97,7 +100,7 @@ function decideWinner(board) {
 function displayBoard() {
   console.clear();
 
-  console.log(`You are ${PLAYER_MARKER}. Computer is ${COMPUTER_MARKER}`);
+  console.log(`You are ${playerMarker}. Computer is ${computerMarker}`);
 
   console.log('');
   console.log('     |     |');
@@ -121,7 +124,7 @@ function emptySquares(board) {
 function findAtRiskSquare(line, board) {
   let markersInLine = line.map(square => board[square]);
 
-  if (markersInLine.filter(val => val === PLAYER_MARKER).length === 2) {
+  if (markersInLine.filter(val => val === playerMarker).length === 2) {
     let openSpot = line.find(square => board[square] === INITIAL_MARKER);
     if (openSpot !== undefined) {
       return openSpot;
@@ -147,6 +150,26 @@ function joinOr(array, delimiter = ', ', final = 'or') {
     " " + final + " " + array.slice(array.length - 1);
 }
 
+function markerSelection(response) {
+  prompt(`Please select your game symbol: 'X' or 'O'.`);
+  response = rdln.question().trim().toUpperCase();
+  while (true) {
+    if (response === 'X') {
+      playerMarker = 'X';
+      computerMarker = 'O';
+      break;
+    } else if (response === 'O') {
+      playerMarker = 'O';
+      computerMarker = 'X';
+      break;
+    } else {
+      prompt(`That is not a valid response. Please select either 'X' or 'O' as your symbol.`);
+      response = rdln.question().trim().toUpperCase();
+    }
+  }
+  return response;
+}
+
 function playerChoosesSquare(board) {
   let square;
   while (true) {
@@ -155,11 +178,11 @@ function playerChoosesSquare(board) {
     if (emptySquares(board).includes(square)) break;
     prompt('Please select a valid square');
   }
-  board[square] = PLAYER_MARKER;
+  board[square] = playerMarker;
 }
 
 function prompt(message) {
-  console.log(`=> ${message}`);
+  console.log(`\n=> ${message}\n`);
 }
 
 function randomizeFirstToPlay() {
@@ -180,13 +203,13 @@ function someoneWon(board) {
   return !!decideWinner(board);
 }
 
-function validContinue() {
+function validContinue(text) {
   let response;
   while (true) {
-    prompt('To continue, hit "C".');
+    prompt(text);
     response = rdln.question().trim().toLowerCase();
     if (response === 'c') break;
-    prompt(`Invalid response. To continue, hit 'C'.`)
+    prompt(`Invalid response. To continue, hit "C".`);
   }
 }
 
@@ -203,13 +226,18 @@ function validReplay() {
       prompt(`Thanks for playing! Bye!`);
       break;
     }
-    prompt(`That is an invalid response. Please type 'Y' to play again, or 'N' to quit.`)
+    prompt(`That is an invalid response. Please type 'Y' to play again, or 'N' to quit.`);
   }
   return response;
 }
 
-
-
+console.clear();
+console.log(`\n${'*'.repeat(23)}\nWelcome to Tic-Tac-Toe!\n${'*'.repeat(23)}\n`);
+markerSelection();
+prompt(`You have been assigned ${playerMarker}. The computer will play using ${computerMarker}.\n 
+For each game, it will be randomized who has the first move.\n
+The first to win 5 games will be declared the ultimate winner!`);
+validContinue(`When ready, please type "C" and the game will begin.`);
 
 while (true) {
 
@@ -217,6 +245,7 @@ while (true) {
   randomizeFirstToPlay();
 
   while (true) {
+
     displayBoard(board);
     chooseSquare(board, currentPlayer);
     currentPlayer = alternatePlayer(currentPlayer);
@@ -226,7 +255,7 @@ while (true) {
   displayBoard(board);
 
   if (boardIsFull(board)) {
-    prompt(`It's a tie!`)
+    prompt(`It's a tie!`);
   }
   if (someoneWon(board)) {
     prompt(`${decideWinner(board)} has won!`);
@@ -236,15 +265,15 @@ while (true) {
   if (decideWinner(board) === 'Player') playerMatchTotal += 1;
   if (decideWinner(board) === 'Computer') computerMatchTotal += 1;
 
-  if (totalGames < 5) {
+  if (totalGames < MAX_WINS) {
     prompt(`You have won ${playerMatchTotal} game(s). Computer has won ${computerMatchTotal} game(s).`);
-    validContinue();
+    validContinue('In order to continue, type "C"');
   }
 
-  if (playerMatchTotal === 5) {
-    prompt(`Congratulations! You were first to 5 games won!`);
+  if (playerMatchTotal === MAX_WINS) {
+    prompt(`Congratulations! You were first to win 5 games!`);
     if (validReplay() === 'n') break;
-  } else if (computerMatchTotal === 5) {
+  } else if (computerMatchTotal === MAX_WINS) {
     prompt(`You've been outmatched! Computer was first to 5 games won. Better luck next time!`);
     if (validReplay() === 'n') break;
   }
